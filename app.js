3a79afd3a4e1285091fe36d005764d1317a17bca
chylex <chylex@fo2.cz>
2015-03-09 16:09:32 +0100

    Basic test site

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
+"use strict";
+
 var app = angular.module("DeliveryEvolution",[
   "ngRoute"
 ]);
 
+app.config([ "$routeProvider", "$locationProvider", "$sce", function($routeProvider, $locationProvider){
+  $routeProvider.when("/",{
+    templateUrl: "app/pages/main.html",
+    controller: "PageMain"
+  });
+  
+  // TODO $locationProvider.html5Mode(true);
+}]);
+
+app.controller("Global",[ "$scope", function($scope){
+  $scope.version = "0.1 dev";
+}]);
+
+app.controller("PageMain",[ "$scope", function($scope){
+  
+}]);
